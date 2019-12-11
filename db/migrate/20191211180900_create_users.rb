946d29a177586889f4844a893dbe86f18823cccc
jonathanbridges <jbridges7@gmail.com>
2019-12-11 10:33:48 -0800

    Finish user model, application controller

--- db/migrate/20191211180900_create_users.rb
+++ db/migrate/20191211180900_create_users.rb
@@ -4,14 +4,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email, null: false
       t.string :name, null: false
       t.boolean :is_employer, null: false, default: false
-      t.integer :company_id,
+      t.integer :company_id
       t.string :password_digest, null:false
       t.string :session_token, null:false
-
       t.timestamps
     end
-    add_index :users, :email
+    add_index :users, :email, unique: true
     add_index :users, :company_id
-    add_index :users, :session_token
+    add_index :users, :session_token, unique: true
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  is_employer     :boolean          default(FALSE), not null
+#  company_id      :integer
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
